eecd6fd70bf60e3f8f5470fd40ba798acc79d681
Robin Seerig <robin@seerig.de>
2017-12-30 15:32:51 +0100

    1.0.5

--- indes.js
+++ indes.js
@@ -9,7 +9,7 @@ const bot = new Discord.Client();
 //get config
 const ownerID = config.ownerID;
 
-//define default variables
+//define default letiables
 const groundCardsDefault = {
     "card1": null,
     "card2": null,
@@ -84,9 +84,15 @@ const cardsDefault = {
     "ace_of_clubs": 0
 };
 
-//set public variables
-var round = -1;
-var dealCards = false;
+//set public letiables
+const gameInfos = {
+  "round": -1,
+  "pot": 0,
+  "call": 0,
+  "currentPlayer": 0,
+  "subRound": false
+}
+let players = [];
 
 //login with token
 bot.login(token.myToken);
@@ -96,6 +102,292 @@ bot.on("ready", () => {
     log("Bot loaded!");
 });
 
+bot.on("message", msg => {
+    if(initMessage(msg) == false){return;}
+    let cmd = msg.content.split(" ")[0]; //get command
+
+    // +-------------------+
+    // |     COMMANDS      |
+    // +-------------------+
+
+    switch (cmd) {
+      case "info":
+        console.log("isOwner: "+ (msg.author.id != ownerID));
+        console.log("round: "+ gameInfos.round);
+        console.log("players: "+ players.length);
+        console.log("players: ");
+        console.log(players);
+        console.log("players: ");
+        console.log(players);
+        break;
+      case "create":
+        if (msg.author.id != ownerID){
+          accessDenied(msg);
+          return;
+        }
+        if (gameInfos.round == -1) {
+            createGame(msg);
+            return;
+        }
+        setupEmbed("Es läuft bereits ein Spiel!");
+        break;
+      case "start":
+        if (msg.author.id != ownerID){
+          accessDenied(msg);
+          return;
+        }
+        if (gameInfos.round > 0) {
+          setupEmbed("Es läuft bereits ein Spiel!");
+          return;
+        }
+        if (gameInfos.round == -1){
+          setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
+          return;
+        }
+        if(players.length >= 2){
+            roundStart(msg);
+            return;
+        }
+        setupEmbed("Mindestens 2 Spieler", players.length+"/2", msg);
+        break;
+      case "stop":
+        if (msg.author.id != ownerID){
+          accessDenied(msg);
+          return;
+        }
+        if (round != -1) {
+            stopGame(msg);
+            return;
+        }
+        setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
+        break;
+      case "join":
+        if (gameInfos.round == 0) {
+            join(msg);
+            return;
+        }
+        setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
+        break;
+      case "clear":
+        if (msg.author.id != ownerID){
+          accessDenied(msg);
+          return;
+        }
+        if(msg.author.id == ownerID){
+          msg.channel.bulkDelete(100);
+        }
+        break;
+      case "call":
+        if (gameInfos.round < 1) {
+          setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
+          return;
+        }
+        if (players[gameInfos.currentPlayer].id != msg.author.id) {
+          return;
+        }
+        callIt(msg);
+        break;
+      default:
+        messageEmbed("Befehle ", "join | Runde beitreten \nready | Karten nehmen \ncall | Mitgehen \nraise <wert> | Erh\u00f6hen \ncheck | Passen (0 Runde) \ninfo | Info's halt.. \nfold | Aussteigen \nall-in | HALLT ALLES", msg);
+        break;
+    }
+});
+
+// +-------------------+
+// |     FUNCTIONS     |
+// +-------------------+
+
+function callIt(msg) {
+    let args = msg.content.split(" ").slice(1); //get arguments
+    if(args[0] === undefined){
+        return;
+    }
+    if (parseInt(args[0]) >= players[gameInfos.currentPlayer].cash) {
+        setupEmbed("Du hast nicht genug Coins", "nutze \"all-in\"", msg);
+        return;
+    }
+
+    gameInfos.pot += subRound ? gameInfos.call - players[gameInfos.currentPlayer].call : gameInfos.call;
+    players[gameInfos.currentPlayer].cash += players[gameInfos.currentPlayer].call - gameInfos.call;
+    players[gameInfos.currentPlayer].call = gameInfos.call;
+
+    messageEmbed("Call", "+" + gameInfos.call + " Coins | (" + gameInfos.pot + " Coins)", msg);
+    manageGame(msg);
+}
+
+function checkGame(msg) {
+  const playersCount = players.length;
+  if (gameInfos.currentPlayer >= (playersCount - 1)) gameInfos.subRound = true;
+  if (gameInfos.subRound) {
+      for (var i = 0; i < (playersCount); i++) {
+          if (players[i].call != gameInfos.call) {
+              gameInfos.currentPlayer = (i)-1;
+              return false;
+          }
+      }
+  }
+  return currPlayer >= (obj.users.length - 1);
+}
+
+
+function manageGame(msg){
+  const playersCount = players.length;
+  for(; gameInfos.currentPlayer < playersCount && players[currentPlayer].state === "out"; ++gameInfos.currentPlayer);
+
+  if (players[gameInfos.currentPlayer].call == gameInfos.call) {
+    manageGame(msg);
+    return;
+  }
+
+  if(gameInfos.currentPlayer >= playersCount){
+    gameInfos.currentPlayer = 0;
+    gameInfos.subRound = true;
+  }
+
+  bot.user.setGame(players[gameInfos.currentPlayer].username + " ist dran!");
+  messageEmbed(players[gameInfos.currentPlayer].username, "Ist an der Reihe", msg);
+}
+
+function getCards(msg) {
+    card1 = getCheckedRandomCard();
+    card2 = getCheckedRandomCard();
+    msg.author.sendMessage("Deine Karten:");
+    msg.author.sendFile(getCardFile(card1));
+    msg.author.sendFile(getCardFile(card2));
+    for (let i = 0; i < (players.length); i++) {
+        if (players[i].id == msg.author.id) {
+            players[i].card1 = card1;
+            players[i].card2 = card2;
+            return;
+        }
+    }
+    return;
+}
+
+//#region ----- CARD    OPERATIONS -----
+
+  //show the flop
+  function flop(msg){
+      card = getCheckedRandomCard();
+      groundCards.card1 = card;
+      msg.channel.sendFile(getCardFile(card));
+
+      card = getCheckedRandomCard();
+      groundCards.card2 = card;
+      msg.channel.sendFile(getCardFile(card));
+
+      card = getCheckedRandomCard();
+      groundCards.card3 = card;
+      msg.channel.sendFile(getCardFile(card));
+  }
+
+  //show the trun
+  function turn(msg){
+      card = getCheckedRandomCard();
+      groundCards.card4 = card;
+      msg.channel.sendFile(getCardFile(card));
+  }
+
+  //show the river
+  function river(msg){
+      card = getCheckedRandomCard();
+      groundCards.card5 = card;
+      msg.channel.sendFile(getCardFile(card));
+  }
+
+  //generate an random card
+  function getRandomCard() {
+      let value = Math.floor(Math.random() * (14 - 2 +1)) + 2;
+      let color = Math.floor(Math.random() * (1 - 4 +1)) + 4;
+      let cardValue;
+      if(value == "11"){cardValue = "jack"}
+      else if(value == "12"){cardValue = "queen"}
+      else if(value == "13"){cardValue = "king"}
+      else if(value == "14"){cardValue = "ace"}
+      else {cardValue = value;}
+
+      if(color == "1"){cardColor = "diamonds"}
+      else if(color == "2"){cardColor = "hearts"}
+      else if(color == "3"){cardColor = "spades"}
+      else if(color == "4"){cardColor = "clubs"}
+
+      let cardName = cardValue+"_of_"+cardColor;
+      return cardName;
+  }
+
+  //generate a checked random card
+  function getCheckedRandomCard(){
+      let cardName = getRandomCard();
+      if (cards[cardName] == 0) {
+          cards[cardName] = 1;
+          return cardName;
+
+      } else {
+          while (true) {
+              if (cards[cardName] == 0) {
+                  cards[cardName] = 1;
+                  return cardName;
+              } else {
+                  let cardName = getRandomCard();
+              }
+          }
+      }
+
+  }
+
+  //get the crad file
+  function getCardFile(n){
+      return "./cards/"+n+".png";
+  }
+
+  //get card text
+  function getCardText(card){
+      let texts = (card + "").split("_")
+      let text = texts[0] + " of " + texts[2]
+      return text;
+  }
+
+//#endregion
+//#region ----- PLAYER  OPERATIONS -----
+
+function join(msg){
+    for (let i = 0; i < (players.length); i++) {
+        if (players[i].id == msg.author.id) {
+            setupEmbed("Achtung", "Du bist bereits beigetreten", msg);
+            return;
+        }
+    }
+    messageEmbed(":spades: " + msg.author.username, "ist beigetreten", msg);
+    players.push({"username": msg.author.username,"id": msg.author.id, "msgObj": msg, "cash": config.startAmount,"state": "join","call": 0 ,"card1":"","card2":""});
+    return;
+}
+
+function check(msg){
+    if(gameInfos.round == -1){
+        setupEmbed("Keine laufende Runde", "", msg);
+        return false;
+    }
+    if (players[gameInfos.currentPlayer].username != msg.author.username) {
+        messageEmbed(players[gameInfos.currentPlayer].username, "ist an der Reihe", msg);
+        return false;
+    }
+    return true;
+}
+
+//check is player is in game
+function isInGame(msg){
+  for (let i = 0; i < (players.length); i++) {
+      if (players[i].id == msg.author.id) {
+          return false;
+      }
+  }
+  return true;
+}
+
+//#endregion
+//#region ----- SETUP   OPERATIONS -----
+
+//pre-checks the message
 function initMessage(msg){
   if(msg.channel.id != config.channelID) {
     return false;
@@ -112,299 +404,52 @@ function initMessage(msg){
   return true;
 }
 
-bot.on("message", msg => {
-    if(initMessage(msg) == false){return;}
-
-    // +-------------------+
-    // |     COMMANDS      |
-    // +-------------------+
-
-    //info of game
-    if(msg.content == "info") {
-        console.log("isOwner: "+ (msg.author.id != ownerID));
-        console.log("round: "+ round);
-        console.log("players: "+ players['users'].length);
-        console.log("players: ");
-        console.log(players['users']);
-        console.log("players: ");
-        console.log(players);
-    }
-
-    //create the game
-    if(msg.content == "create") {
-        if (msg.author.id != ownerID){
-          accessDenied(msg);
-          return;
-        }
-        if (round == -1) {
-            startGame(msg);
-            return;
-        }
-        setupEmbed("Es läuft bereits ein Spiel!")
-        return;
-    }
-
-    //start the game
-    if(msg.content == "start") {
-        if (msg.author.id != ownerID){
-          accessDenied(msg);
-          return;
-        }
-        if (round > 0) {
-          setupEmbed("Es läuft bereits ein Spiel!");
-          return;
-        }
-        if (round == -1){
-          setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
-          return;
-        }
-        if(players['users'].length >= 2){
-            roundStart(msg);
-            return;
-        }
-        setupEmbed("Mindestens 2 Spieler", players['users'].length+"/2", msg);
-        return;
-    }
-
-    //stop the game
-    if(msg.content == "stop") {
-        if (msg.author.id != ownerID){
-          accessDenied(msg);
-          return;
-        }
-        if (round != -1) {
-            stopGame(msg);
-            return;
-        }
-        setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
-        return;
-    }
-
-    //start the game
-    if(msg.content == "join") {
-        if (round == 0) {
-            join(msg);
-            getCards(msg);
-        }
-        setupEmbed("Achtung", "Es läuft aktuell keine Runde!", msg);
-        return;
-    }
-
-    //remove all recent messages
-    if (msg.content == "clear") {
-        if (msg.author.id != ownerID){
-          accessDenied(msg);
-          return;
-        }
-        if(msg.author.id == ownerID){
-          msg.channel.bulkDelete(100);
-        }
-        return;
-    }
-    return;
-});
-
-// +-------------------+
-// |     FUNCTIONS     |
-// +-------------------+
-
-function manageGame(msg){
-  players = players['users'].length;
-}
-
-function getCards(msg) {
-    card1 = getCheckedRandomCard();
-    card2 = getCheckedRandomCard();
-    msg.author.sendMessage("Deine Karten:");
-    msg.author.sendFile(getCardFile(card1));
-    msg.author.sendFile(getCardFile(card2));
-    for (var i = 0; i < (obj.users.length); i++) {
-        if (players['users'][i].id == msg.author.id) {
-            players['users'][i].card1 = card1;
-            players['users'][i].card2 = card2;
-            return;
-        }
-    }
-    return;
-}
-
-
-//----- CARD OPERATIONS -----
-
-//show the flop
-function flop(msg){
-    card = getCheckedRandomCard();
-    groundCards.card1 = card;
-    msg.channel.sendFile(getCardFile(card));
-
-    card = getCheckedRandomCard();
-    groundCards.card2 = card;
-    msg.channel.sendFile(getCardFile(card));
-
-    card = getCheckedRandomCard();
-    groundCards.card3 = card;
-    msg.channel.sendFile(getCardFile(card));
-}
-
-//show the trun
-function turn(msg){
-    card = getCheckedRandomCard();
-    groundCards.card4 = card;
-    msg.channel.sendFile(getCardFile(card));
-}
-
-//show the river
-function river(msg){
-    card = getCheckedRandomCard();
-    groundCards.card5 = card;
-    msg.channel.sendFile(getCardFile(card));
-}
-
-//generate an random card
-function getRandomCard() {
-    var value = Math.floor(Math.random() * (14 - 2 +1)) + 2;
-    var color = Math.floor(Math.random() * (1 - 4 +1)) + 4;
-    var cardValue;
-    if(value == "11"){cardValue = "jack"}
-    else if(value == "12"){cardValue = "queen"}
-    else if(value == "13"){cardValue = "king"}
-    else if(value == "14"){cardValue = "ace"}
-    else {cardValue = value;}
-
-    if(color == "1"){cardColor = "diamonds"}
-    else if(color == "2"){cardColor = "hearts"}
-    else if(color == "3"){cardColor = "spades"}
-    else if(color == "4"){cardColor = "clubs"}
-
-    var cardName = cardValue+"_of_"+cardColor;
-    return cardName;
-}
-
-//generate a checked random card
-function getCheckedRandomCard(){
-    var cardName = getRandomCard();
-    if (cards[cardName] == 0) {
-        cards[cardName] = 1;
-        return cardName;
-
-    } else {
-        while (true) {
-            if (cards[cardName] == 0) {
-                cards[cardName] = 1;
-                return cardName;
-            } else {
-                var cardName = getRandomCard();
-            }
-        }
-    }
-
-}
-
-//get the crad file
-function getCardFile(n){
-    return "./cards/"+n+".png";
-}
-
-//get card text
-function getCardText(card){
-    var texts = (card + "").split("_")
-    var text = texts[0] + " of " + texts[2]
-    return text;
-}
-
-//----- PLAYER OPERATIONS -----
-
-function join(msg){
-    for (var i = 0; i < (players['users'].length); i++) {
-        if (players['users'][i].id == msg.author.id) {
-            setupEmbed("Achtung", "Du bist bereits beigetreten", msg);
-            return;
-        }
-    }
-    messageEmbed(":spades: " + msg.author.username, "ist beigetreten", msg);
-    players['users'].push({"username": msg.author.username,"id": msg.author.id,"cash": config.startAmount,"state": "join","call": 0 ,"card1":"","card2":""});
-    return;
-}
-
-function check(msg){
-    if(round == -1){
-        setupEmbed("Keine laufende Runde", "", msg);
-        return false;
-    }
-    if (players['users'][currentPlayer].username != msg.author.username) {
-        messageEmbed(players['users'][currentPlayer].username, "ist an der Reihe", msg);
-        return false;
-    }
-    return true;
-}
-
-//check is player is in game
-function isInGame(msg){
-  for (var i = 0; i < (players['users'].length); i++) {
-      if (players['users'][i].id == msg.author.id) {
-          return false;
-      }
-  }
-  return true;
-}
-
-//----- SETUP OPERATIONS -----
-
 //start the round
 function roundStart(msg){
-    round++;
+    gameInfos.round++;
     groundCards = groundCardsDefault;
     cards = cardsDefault;
-    getCards(msg);
-    dealCards = true;
+    for (let i = 0; i < (players.length); i++) {
+      getCards(players[i].msgObj);
+    }
+
+    bot.user.setGame(players[gameInfos.currentPlayer].username + " ist dran!");
+    messageEmbed(players[gameInfos.currentPlayer].username, "Ist an der Reihe", msg);
 }
 
 //stop the round
 function roundEnd(){
-    dealCards = false;
-    round++;
+    gameInfos.round++;
 }
 
 //start a new game
-function startGame(msg){
-    players = {
-      "users": []
-    };
-    while(!isInGame(msg)){
-      players = {
-        "users": []
-      };
-      delete players['users'];
-      players = {
-        "users": []
-      };
-    }
+function createGame(msg){
+    players = [];
     msg.channel.bulkDelete(100);
     messageEmbed(":spades: Spiel gestartet", "von " + msg.author.username, msg);
-    players['users'].push({"username": msg.author.username, "id": msg.author.id, "cash": config.startAmount, "state": "dealer", "call": 0, "card1": "", "card2": ""});
-    round = 0;
-    currentPlayer = 0;
-    return;
+    players.push({"username": msg.author.username, "id": msg.author.id, "msgObj": msg, "cash": config.startAmount, "state": "dealer", "call": 0, "card1": "", "card2": ""});
+    gameInfos.round = 0;
+    gameInfos.currentPlayer = 0;
 }
 
 //stop the game
 function stopGame(msg){
     log("Stop Game!");
     setupEmbed("Runde gestoppt", "von " + msg.author.username, msg);
-    players = {};
+    players = [];
     cards = {};
     groundCards = {};
-    currentPlayer = 0;
-    round = -1;
-    return;
+    gameInfos.currentPlayer = 0;
+    gameInfos.round = -1;
 }
 
-//----- MESSAGE OPERATIONS -----
+//#endregion
+//#region ----- MESSAGE OPERATIONS -----
 
 
 //message service
 function messageEmbed(t, d, msg) {
-    var embedPoker = new Discord.RichEmbed();
+    let embedPoker = new Discord.RichEmbed();
     embedPoker.setTitle(t);
     embedPoker.setDescription(d);
     embedPoker.setColor("#55FF55");
@@ -413,7 +458,7 @@ function messageEmbed(t, d, msg) {
 
 //message service
 function setupEmbed(t, d, msg) {
-    var embedPoker = new Discord.RichEmbed();
+    let embedPoker = new Discord.RichEmbed();
     embedPoker.setTitle(t);
     embedPoker.setDescription(d);
     embedPoker.setColor("#FFB700");
@@ -422,7 +467,7 @@ function setupEmbed(t, d, msg) {
 
 //message service
 function accessDenied(msg) {
-    var embedPoker = new Discord.RichEmbed();
+    let embedPoker = new Discord.RichEmbed();
     embedPoker.setTitle("Verweigert!");
     embedPoker.setDescription(msg.author.username + ", du hast keine Rechte dafür.");
     embedPoker.setColor("#FF0000");
@@ -433,3 +478,5 @@ function accessDenied(msg) {
 function log(msg){
     console.log("["+Math.round((new Date()).getTime() / 1000)+"] "+msg);
 }
+
+//#endregion
